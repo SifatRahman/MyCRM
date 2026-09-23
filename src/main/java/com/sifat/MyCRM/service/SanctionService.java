fd60e5c280f1e6919fb713dd2fa6e165343003fa
Sifat <shifaturrahman390@gmail.com>
2026-09-23 23:25:44 +0600

    call reduced

--- src/main/java/com/sifat/MyCRM/service/SanctionService.java
+++ src/main/java/com/sifat/MyCRM/service/SanctionService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -25,73 +26,75 @@ public class SanctionService extends BaseService {
         USSanctionListDataOutDTO usSanctionListDataOutDTO = sanctionXmlService.parseXmlFile(inputStream);
         List<USSanctionIndividualDataDTO> individualXmlDataDTOs = usSanctionListDataOutDTO.getIndividuals();
 
-        return individualXmlDataDTOs.stream().map(individualXmlDataDTO -> {
-            SanctionIndividual sanctionIndividual = new SanctionIndividual();
-            sanctionIndividual.setId(getUUID());
-            sanctionIndividual.setDataId(individualXmlDataDTO.getData_id());
-            sanctionIndividual.setVersionNo(individualXmlDataDTO.getVersion_no());
-            sanctionIndividual.setFirstName(individualXmlDataDTO.getFirst_name());
-            sanctionIndividual.setSecondName(individualXmlDataDTO.getSecond_name());
-            sanctionIndividual.setThirdName(individualXmlDataDTO.getThird_name());
-            sanctionIndividual.setFourthName(individualXmlDataDTO.getFourth_name());
-            sanctionIndividual.setUnListType(individualXmlDataDTO.getUn_list_type());
-            sanctionIndividual.setReferenceNumber(individualXmlDataDTO.getReference_number());
-            sanctionIndividual.setListedOn(individualXmlDataDTO.getListed_on());
-            sanctionIndividual.setNameOriginalScript(individualXmlDataDTO.getName_original_script());
-            sanctionIndividual.setComments1(individualXmlDataDTO.getComments1());
-            sanctionIndividual.setHasInterpolLink(individualXmlDataDTO.getHas_interpol_link());
-            sanctionIndividual.setInterpolLink(individualXmlDataDTO.getInterpol_link());
+        List<SanctionIndividual> savingIndividuals = Stream.of(individualXmlDataDTOs.getFirst())
+                .map(individualXmlDataDTO -> {
+                    SanctionIndividual sanctionIndividual = new SanctionIndividual();
+                    sanctionIndividual.setId(getUUID());
+                    sanctionIndividual.setDataId(individualXmlDataDTO.getData_id());
+                    sanctionIndividual.setVersionNo(individualXmlDataDTO.getVersion_no());
+                    sanctionIndividual.setFirstName(individualXmlDataDTO.getFirst_name());
+                    sanctionIndividual.setSecondName(individualXmlDataDTO.getSecond_name());
+                    sanctionIndividual.setThirdName(individualXmlDataDTO.getThird_name());
+                    sanctionIndividual.setFourthName(individualXmlDataDTO.getFourth_name());
+                    sanctionIndividual.setUnListType(individualXmlDataDTO.getUn_list_type());
+                    sanctionIndividual.setReferenceNumber(individualXmlDataDTO.getReference_number());
+                    sanctionIndividual.setListedOn(individualXmlDataDTO.getListed_on());
+                    sanctionIndividual.setNameOriginalScript(individualXmlDataDTO.getName_original_script());
+                    sanctionIndividual.setComments1(individualXmlDataDTO.getComments1());
+                    sanctionIndividual.setHasInterpolLink(individualXmlDataDTO.getHas_interpol_link());
+                    sanctionIndividual.setInterpolLink(individualXmlDataDTO.getInterpol_link());
 
-            List<String> xmlDesignations = individualXmlDataDTO.getDesignation();
-            ArrayList<SanctionIndividualDesignation> sanctionIndividualDesignations = new ArrayList<>();
-            xmlDesignations.forEach(e -> {
-                SanctionIndividualDesignation sanctionIndividualDesignation =
-                        SanctionIndividualDesignation.builder()
-                                .id(getUUID())
-                                .individual(sanctionIndividual)
-                                .designation(e)
-                                .build();
-                sanctionIndividualDesignations.add(sanctionIndividualDesignation);
-            });
+                    List<String> xmlDesignations = individualXmlDataDTO.getDesignation();
+                    ArrayList<SanctionIndividualDesignation> sanctionIndividualDesignations = new ArrayList<>();
+                    xmlDesignations.forEach(e -> {
+                        SanctionIndividualDesignation sanctionIndividualDesignation =
+                                SanctionIndividualDesignation.builder()
+                                        .id(getUUID())
+                                        .individual(sanctionIndividual)
+                                        .designation(e)
+                                        .build();
+                        sanctionIndividualDesignations.add(sanctionIndividualDesignation);
+                    });
 
-            sanctionIndividual.setDesignations(sanctionIndividualDesignations);
+                    sanctionIndividual.setDesignations(sanctionIndividualDesignations);
 
 
-            //nationalities
-            List<String> xmlNationalities = individualXmlDataDTO.getNationality();
-            ArrayList<SanctionIndividualNationality> nationalities = new ArrayList<>();
-            xmlNationalities.forEach(e -> {
-                SanctionIndividualNationality nationality =
-                        SanctionIndividualNationality.builder()
-                                .id(getUUID())
-                                .individual(sanctionIndividual)
-                                .nationality(e)
-                                .build();
-                nationalities.add(nationality);
-            });
-            sanctionIndividual.setNationalities(nationalities);
+                    //nationalities
+                    List<String> xmlNationalities = individualXmlDataDTO.getNationality();
+                    ArrayList<SanctionIndividualNationality> nationalities = new ArrayList<>();
+                    xmlNationalities.forEach(e -> {
+                        SanctionIndividualNationality nationality =
+                                SanctionIndividualNationality.builder()
+                                        .id(getUUID())
+                                        .individual(sanctionIndividual)
+                                        .nationality(e)
+                                        .build();
+                        nationalities.add(nationality);
+                    });
+                    sanctionIndividual.setNationalities(nationalities);
 
-            //listTypes
-            List<String> xmlDataDTOListType = individualXmlDataDTO.getList_type();
-            ArrayList<SanctionIndividualListType> listTypes = new ArrayList<>();
-            xmlDataDTOListType.forEach(e -> {
-                SanctionIndividualListType listType =
-                        SanctionIndividualListType.builder()
-                                .id(getUUID())
-                                .individual(sanctionIndividual)
-                                .list_type(e)
-                                .build();
-                listTypes.add(listType);
-            });
-            sanctionIndividual.setListTypes(listTypes);
+                    //listTypes
+                    List<String> xmlDataDTOListType = individualXmlDataDTO.getList_type();
+                    ArrayList<SanctionIndividualListType> listTypes = new ArrayList<>();
+                    xmlDataDTOListType.forEach(e -> {
+                        SanctionIndividualListType listType =
+                                SanctionIndividualListType.builder()
+                                        .id(getUUID())
+                                        .individual(sanctionIndividual)
+                                        .list_type(e)
+                                        .build();
+                        listTypes.add(listType);
+                    });
+                    sanctionIndividual.setListTypes(listTypes);
 
-            //remaining
+                    //remaining
 //        sanctionIndividual.setDesignations(sanctionIndividualDesignations);
 //        sanctionIndividual.setDesignations(sanctionIndividualDesignations);
 //        sanctionIndividual.setDesignations(sanctionIndividualDesignations);
 //        sanctionIndividual.setDesignations(sanctionIndividualDesignations);
 
-             return sanctionIndividualRepository.save(sanctionIndividual);
-        }).toList();
+                    return sanctionIndividual;
+                }).toList();
+        return sanctionIndividualRepository.saveAll(savingIndividuals);
     }
 }
